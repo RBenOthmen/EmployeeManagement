53d017a83386741f732ff786c07c473847702c51
rami ben othmen <ramibenothmen@gmail.com>
2023-01-25 01:31:35 +0100

    Initial commit

---
+++ src/main/java/tech/getarrays/employeemanaager/EmployeemanaagerApplication.java
@@ -0,0 +1,13 @@
+package tech.getarrays.employeemanaager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeemanaagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeemanaagerApplication.class, args);
+	}
+
+}
